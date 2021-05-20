4072d5242e0622c3607e95640540b4107ed51fc5
NiPeGun <nipegun@gmail.com>
2021-05-20 11:39:45 +0200

    Create monero-daemon-iniciar.sh

---
+++ monero-daemon-iniciar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Iniciando monerod..."
+echo ""
+$CarpetaHome/CoresCrypto/XMR/bin/monerod --detach
+
