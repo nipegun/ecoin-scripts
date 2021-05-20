faa7b04494211ead2df65d016abc19238e47632c
NiPeGun <nipegun@gmail.com>
2021-05-20 11:28:50 +0200

    Create raven-qt-iniciar.sh

---
+++ raven-qt-iniciar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Iniciando raven-qt..."
+echo ""
+$CarpetaHome/scripts/c-scripts/raven-daemon-parar.sh
+sleep 5
+/home/$UsuarioDaemon/CoresCrypto/RVN/bin/raven-qt -min -testnet=0 -regtest=0
+
