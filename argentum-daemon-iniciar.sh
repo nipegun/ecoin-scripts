7319dd7a38533173e149d0b5126963d08037b0b1
NiPeGun <nipegun@gmail.com>
2021-05-20 12:15:47 +0200

    Update argentum-daemon-iniciar.sh

--- argentum-daemon-iniciar.sh
+++ argentum-daemon-iniciar.sh
@@ -5,9 +5,9 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------
-#  Script de NiPeGun para 
-#-----------------------------------------------------------
+#---------------------------------------------------------
+#  Script de NiPeGun para iniciar el demonio de argentum
+#---------------------------------------------------------
 
 ## Ver si la variable de entorno HOME termina con una /
    CarpetaHome="$HOME"
