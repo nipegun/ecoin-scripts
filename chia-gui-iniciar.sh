512db358359fd11ab2faadf71d7f3ea0c69a16bc
NiPeGun <nipegun@gmail.com>
2021-05-20 11:42:19 +0200

    Create chia-gui-iniciar.sh

---
+++ chia-gui-iniciar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Iniciando chia-blockchain..."
+echo ""
+$CarpetaHome/scripts/c-scripts/chia-daemon-parar.sh
+sleep 5
+$CarpetaHome/CoresCrypto/XCH/bin/chia-blockchain %U
+
