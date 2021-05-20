59a9c16ef58f05afe7c1dd2581876488eba56e9e
NiPeGun <nipegun@gmail.com>
2021-05-20 11:29:01 +0200

    Create raven-daemon-parar.sh

---
+++ raven-daemon-parar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Parando el daemon ravend..."
+echo ""
+$CarpetaHome/CoresCrypto/RVN/bin/raven-cli stop
+
