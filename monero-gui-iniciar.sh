75e2b84c58fa9ac41e3c9aa7eb9c1a499d5adcf1
NiPeGun <nipegun@gmail.com>
2021-05-20 11:39:51 +0200

    Create monero-gui-iniciar.sh

---
+++ monero-gui-iniciar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Iniciando monero-gui..."
+echo ""
+$CarpetaHome/scripts/c-scripts/monero-daemon-parar.sh
+sleep 5
+$CarpetaHome/CoresCrypto/XMR/bin/monero-wallet-gui %u
+
