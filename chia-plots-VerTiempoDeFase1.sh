4779012d0dcd5afe28a9736f86376508855f2de0
NiPeGun <nipegun@gmail.com>
2021-05-20 12:08:09 +0200

    Create chia-plots-VerTiempoDeFase1.sh

---
+++ chia-plots-VerTiempoDeFase1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-----------------------------------------------------------------------
+#  Script de NiPeGun para visualizar lo que tarda la fase 1 del ploteo
+#-----------------------------------------------------------------------
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+find $CarpetaHome/.chia/mainnet/plotter -type f -exec cat {} \; | grep "ime for phase 1"
+
