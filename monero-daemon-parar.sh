58660cedb332a5f78ae39ca2f7e6e02d1ac7fc52
NiPeGun <nipegun@gmail.com>
2021-05-20 11:39:47 +0200

    Create monero-daemon-parar.sh

---
+++ monero-daemon-parar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Parando monerod..."
+echo ""
+$CarpetaHome/CoresCrypto/XMR/bin/monerod stop_daemon
+
