78be42cb0631f6a1e34ec5cf8e7567b06e855f9d
NiPeGun <nipegun@gmail.com>
2021-05-20 11:11:28 +0200

    Create litecoin-daemon-iniciar.sh

---
+++ litecoin-daemon-iniciar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Iniciando el daemon litecoind..."
+echo ""
+$CarpetaHome/CoresCrypto/LTC/bin/litecoind -daemon
+
