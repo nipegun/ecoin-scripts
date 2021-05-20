0ffe39c3c61f3b18e74c5120158c2a32e07dc47a
NiPeGun <nipegun@gmail.com>
2021-05-20 11:35:19 +0200

    Create argentum-qt-iniciar.sh

---
+++ argentum-qt-iniciar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Iniciando argentum-qt..."
+echo ""
+$CarpetaHome/scripts/c-scripts/argentum-daemon-parar.sh
+sleep 5
+$CarpetaHome/CoresCrypto/ARG/bin/argentum-qt
+
