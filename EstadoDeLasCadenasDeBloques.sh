db37f81a94f35da3301ee6ad257badcb3429c359
NiPeGun <nipegun@gmail.com>
2021-08-28 15:10:46 +0200

    Update EstadoDeLasCadenasDeBloques.sh

--- EstadoDeLasCadenasDeBloques.sh
+++ EstadoDeLasCadenasDeBloques.sh
@@ -3,14 +3,28 @@
 Usuario=pooladmin
 Salto=%0A
 
-ltcHeaders=$(/home/pooladmin/Cryptos/LTC/bin/litecoin-cli getblockchaininfo | jq .headers)
- ltcBlocks=$(/home/pooladmin/Cryptos/LTC/bin/litecoin-cli getblockchaininfo | jq .blocks)
-rvnHeaders=$(/home/pooladmin/Cryptos/RVN/bin/raven-cli getblockchaininfo | jq .headers)
- rvnBlocks=$(/home/pooladmin/Cryptos/RVN/bin/raven-cli getblockchaininfo | jq .blocks)
-argHeaders=$(/home/pooladmin/Cryptos/ARG/bin/argentum-cli getblockchaininfo | jq .headers)
- argBlocks=$(/home/pooladmin/Cryptos/ARG/bin/argentum-cli getblockchaininfo | jq .blocks)
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
 
-/home/pooladmin/scripts/TelegramearHTML.sh "\
+ltcHeaders=$($CarpetaHome/Cryptos/LTC/bin/litecoin-cli getblockchaininfo | jq .headers)
+ ltcBlocks=$($CarpetaHome/Cryptos/LTC/bin/litecoin-cli getblockchaininfo | jq .blocks)
+rvnHeaders=$($CarpetaHome/Cryptos/RVN/bin/raven-cli getblockchaininfo | jq .headers)
+ rvnBlocks=$($CarpetaHome/Cryptos/RVN/bin/raven-cli getblockchaininfo | jq .blocks)
+argHeaders=$($CarpetaHome/Cryptos/ARG/bin/argentum-cli getblockchaininfo | jq .headers)
+ argBlocks=$($CarpetaHome/Cryptos/ARG/bin/argentum-cli getblockchaininfo | jq .blocks)
+
+#ltcHeaders=$(/home/pooladmin/Cryptos/LTC/bin/litecoin-cli getblockchaininfo | jq .headers)
+# ltcBlocks=$(/home/pooladmin/Cryptos/LTC/bin/litecoin-cli getblockchaininfo | jq .blocks)
+#rvnHeaders=$(/home/pooladmin/Cryptos/RVN/bin/raven-cli getblockchaininfo | jq .headers)
+# rvnBlocks=$(/home/pooladmin/Cryptos/RVN/bin/raven-cli getblockchaininfo | jq .blocks)
+#argHeaders=$(/home/pooladmin/Cryptos/ARG/bin/argentum-cli getblockchaininfo | jq .headers)
+# argBlocks=$(/home/pooladmin/Cryptos/ARG/bin/argentum-cli getblockchaininfo | jq .blocks)
+
+$CarpetaHome/scripts/TelegramearHTML.sh "\
 <b>Estado de los nodos cripto:</b>$Salto $Salto\
 <i>Nodo Litecoin:</i>$Salto $Salto\
  Bloques totales: $ltcHeaders $Salto\
