457a670b9aa8dbacefeab2cf1567032d3e29422b
NiPeGun <nipegun@gmail.com>
2022-01-20 21:40:06 +0100

    Update crp-minero-resetear-en-root-ei.sh

--- crp-minero-resetear-en-root-ei.sh
+++ crp-minero-resetear-en-root-ei.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para resetear el minero de utopia en el usuario root de Debian
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/c-scripts/main/crp-minero-resetear-en-root.sh | bash
-#----------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/c-scripts/main/crp-minero-resetear-en-root-ei.sh | bash
+#-----------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
