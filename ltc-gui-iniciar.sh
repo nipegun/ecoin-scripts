3ba9d62b873f574492dd4cdbc3a7bae11551b33a
NiPeGun <nipegun@gmail.com>
2022-03-01 12:40:51 +0100

    Create ltc-gui-iniciar.sh

---
+++ ltc-gui-iniciar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-----------------------------------------------------------------
+#  Script de NiPeGun para iniciar la cartera gráfica de litecoin
+#-----------------------------------------------------------------
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Iniciando litecoin-qt..."
+echo ""
+$CarpetaHome/scripts/c-scripts/ltc-daemon-parar.sh
+sleep 5
+$CarpetaHome/Cryptos/LTC/bin/litecoin-qt
+
