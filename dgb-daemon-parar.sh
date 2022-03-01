a58ba7b532de5ff9e836362f17c7c4b1059b333f
NiPeGun <nipegun@gmail.com>
2022-03-01 11:35:57 +0100

    Create dgb-daemon-parar.sh

---
+++ dgb-daemon-parar.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#------------------------------------------------------
+#  Script de NiPeGun para parar el daemon de digibyte
+#------------------------------------------------------
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Parando el daemon digibyted..."
+echo ""
+$CarpetaHome/Cryptos/DGB/bin/digibyte-cli stop
