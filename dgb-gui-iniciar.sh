aa59606d5ace498e942f2b2da99d2e6071592fdb
NiPeGun <nipegun@gmail.com>
2022-03-01 11:34:39 +0100

    Create dgb-gui-iniciar.sh

---
+++ dgb-gui-iniciar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-----------------------------------------------------------------
+#  Script de NiPeGun para iniciar la cartera gráfica de digibyte
+#-----------------------------------------------------------------
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Iniciando digibyte-qt..."
+echo ""
+$CarpetaHome/scripts/c-scripts/dgb-daemon-parar.sh
+sleep 5
+$CarpetaHome/Cryptos/DGB/bin/digibyte-qt
