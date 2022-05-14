eec34fbd4412f3e25fe7c4d7fe8c1cb78f12b0e4
NiPeGun <nipegun@gmail.com>
2022-05-14 23:31:49 +0200

    Create xch-wallet-transacciones.sh

---
+++ xch-wallet-transacciones.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para mostrar las transacciones de la cartera de chia
+# ----------
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Mostrando transacciones de la wallet de chia..."
+echo ""
+$CarpetaHome/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia wallet get_transactions
+
