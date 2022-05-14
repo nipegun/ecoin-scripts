1ced671336bfc9ed34e9ccd18b72e8db7e11c614
NiPeGun <nipegun@gmail.com>
2022-05-14 23:31:02 +0200

    Update xch-wallet-version.sh

--- xch-wallet-version.sh
+++ xch-wallet-version.sh
@@ -17,7 +17,7 @@
    fi
 
 echo ""
-echo "  Estado del nodo de Chia..."
+echo "  Versión de la cartera de Chia..."
 echo ""
 $CarpetaHome/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia version
 
