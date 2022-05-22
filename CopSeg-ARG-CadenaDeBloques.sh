4335b0ee5b520fd807a921c0f5e92b13f95fd7bd
NiPeGun <nipegun@gmail.com>
2022-05-22 14:10:18 +0200

    Create CopSeg-ARG-CadenaDeBloques.sh

---
+++ CopSeg-ARG-CadenaDeBloques.sh
@@ -0,0 +1,17 @@
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para sincronizar los c-scripts
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/c-scripts/main/CScripts-Sincronizar.sh | bash
+#-------------------------------------------------------------------------------------------------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
