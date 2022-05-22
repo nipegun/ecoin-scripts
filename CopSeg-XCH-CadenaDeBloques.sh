3c6f0065d3a287ccdbb2847e8e63e6fd647794ca
NiPeGun <nipegun@gmail.com>
2022-05-22 14:25:48 +0200

    Update and rename CopSeg-ARG-CadenaDeBloques.sh to CopSeg-XCH-CadenaDeBloques.sh

--- CopSeg-XCH-CadenaDeBloques.sh
+++ CopSeg-XCH-CadenaDeBloques.sh
@@ -5,7 +5,7 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 #-------------------------------------------------------------------------------------------------
-#  Script de NiPeGun para sincronizar los c-scripts
+#  Script de NiPeGun para hacer copia de seguridad de la cadena de bloques de chia
 #
 #  Ejecución remota:
 #  curl -s https://raw.githubusercontent.com/nipegun/c-scripts/main/CScripts-Sincronizar.sh | bash
@@ -14,4 +14,23 @@
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
 FinColor='\033[0m'
+
+vFechaCopia=
+vCarpetaUsuario=
+vUbicBD="$vCarpetaUsuario"
+vCarpDestinoCopSeg="/CopSeg/Chia/$vFechaCopia/.chia/mainnet/db"
+vVersRedPrinc=v2
+mkdir -p $vCarpDestinoCopSeg 2> /dev/null
+
+# Comprobar si el paquete sqlite está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s sqlite 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo "  sqlite no está instalado. Iniciando su instalación..."
+    echo ""
+    apt-get -y update > /dev/null
+    apt-get -y install sqlite
+    echo ""
+  fi
+
+sqlite3 $vCarpetaHome/.chia/mainnet/db/blockchain_$vVersRedPrinc_mainnet.sqlite "vacuum into '$vCarpDestinoCopSeg/blockchain_$vVersRedPrinc_mainnet.sqlite'"
 
