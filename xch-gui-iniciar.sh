79d8ef4190a6326ee649ce42cf6a10902d76d3d2
NiPeGun <nipegun@gmail.com>
2022-05-29 01:58:00 +0200

    Update xch-gui-iniciar.sh

--- xch-gui-iniciar.sh
+++ xch-gui-iniciar.sh
@@ -19,7 +19,9 @@
 echo ""
 echo "  Iniciando chia-blockchain..."
 echo ""
+chmod +x $CarpetaHome/scripts/c-scripts/xch-daemon-parar.sh 2> /dev/null
 $CarpetaHome/scripts/c-scripts/xch-daemon-parar.sh
 sleep 5
+chmod +x $CarpetaHome/Cryptos/XCH/chia-blockchain/chia-blockchain 2> /dev/null
 $CarpetaHome/Cryptos/XCH/chia-blockchain/chia-blockchain
 
