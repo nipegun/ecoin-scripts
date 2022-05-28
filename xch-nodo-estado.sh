db5fb58ae65cbeaf9a30bb49ebadf29c6b03da44
NiPeGun <nipegun@gmail.com>
2022-05-29 01:57:58 +0200

    Update xch-nodo-estado.sh

--- xch-nodo-estado.sh
+++ xch-nodo-estado.sh
@@ -19,5 +19,6 @@
 echo ""
 echo "  Estado del nodo de Chia..."
 echo ""
+chmod +x $CarpetaHome/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia 2> /dev/null
 $CarpetaHome/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia show -s
 
