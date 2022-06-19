b002f4dbbb290804f6cdb8486576bfbd45991c90
NiPeGun <nipegun@gmail.com>
2022-06-19 22:10:51 +0200

    Update xch-CopSeg-CadenaDeBloques-ConNodoApagado.sh

--- xch-CopSeg-CadenaDeBloques-ConNodoApagado.sh
+++ xch-CopSeg-CadenaDeBloques-ConNodoApagado.sh
@@ -9,7 +9,7 @@
 #  Script de NiPeGun para hacer copia de seguridad de la cadena de bloques de chia con el nodo apagado
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/c-scripts/main/xch-CopSeg-CadenaDeBloques-ConNodoEncendido.sh | bash
+#  curl -s https://raw.githubusercontent.com/nipegun/c-scripts/main/xch-CopSeg-CadenaDeBloques-ConNodoApagado.sh | bash
 # ----------
 
 ColorRojo='\033[1;31m'
