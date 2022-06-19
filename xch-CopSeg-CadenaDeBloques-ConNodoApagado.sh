6b0bfaada92db011215c1560d1b73ed883ccd038
NiPeGun <nipegun@gmail.com>
2022-06-19 22:10:19 +0200

    Create xch-CopSeg-CadenaDeBloques-ConNodoApagado.sh

---
+++ xch-CopSeg-CadenaDeBloques-ConNodoApagado.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para hacer copia de seguridad de la cadena de bloques de chia con el nodo apagado
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/c-scripts/main/xch-CopSeg-CadenaDeBloques-ConNodoEncendido.sh | bash
+# ----------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+FechaDeEjecCopSeg=$(date +A%Y-M%m-D%d@%T)
+
+Usuario="nipegun"
+CarpetaBD="/home/$Usuario/.chia/mainnet/db/"
+
+echo ""
+echo -e "${ColorVerde}  Iniciando la copia de seguridad de la base de datos del nodo Chia...${FinColor}"
+echo ""
+
+mkdir -p /Host/XCH/CopSegBD/$FechaDeEjecCopSeg 2> /dev/null
+
+# Comprobar si el paquete rsync está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s rsync 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo "  rsync no está instalado. Iniciando su instalación..."
+    echo ""
+    apt-get -y update
+    apt-get -y install rsync
+    echo ""
+  fi
+#rsync -a --delete /Discos/HDD-CopSeg/ /Discos/HDD-CopSegExt
+
+rsync -av $CarpetaBD /Host/XCH/CopSegBD/$FechaDeEjecCopSeg
+
