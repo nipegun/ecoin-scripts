4b965db90a184a0c8f9a7158947e7f30bc4c97c4
NiPeGun <nipegun@gmail.com>
2022-06-19 22:11:08 +0200

    Update xch-CopSeg-CadenaDeBloques-ConNodoEncendido.sh

--- xch-CopSeg-CadenaDeBloques-ConNodoEncendido.sh
+++ xch-CopSeg-CadenaDeBloques-ConNodoEncendido.sh
@@ -6,7 +6,7 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-#  Script de NiPeGun para hacer copia de seguridad de la cadena de bloques de chia
+#  Script de NiPeGun para hacer copia de seguridad de la cadena de bloques de chia con el nodo encendido
 #
 #  Ejecución remota:
 #  curl -s https://raw.githubusercontent.com/nipegun/c-scripts/main/xch-CopSeg-CadenaDeBloques-ConNodoEncendido.sh | bash
