a9b0085ef5db7b9da9819005c0add05d10b5d000
NiPeGun <nipegun@gmail.com>
2022-07-14 13:27:35 +0200

    Create rtm-daemon-parar.sh

---
+++ rtm-daemon-parar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#------------------------------------------------------
+#  Script de NiPeGun para parar el daemon de litecoin
+#------------------------------------------------------
+
+## Ver si la variable de entorno HOME termina con una /
+   CarpetaHome="$HOME"
+   if [[ "$CarpetaHome" == */ ]]; then
+     # Quitarle la /
+     CarpetaHome=${CarpetaHome%?}
+   fi
+
+echo ""
+echo "  Parando el daemon litecoind..."
+echo ""
+chmod +x $CarpetaHome/Cryptos/LTC/bin/litecoin-cli 2> /dev/null
+$CarpetaHome/Cryptos/LTC/bin/litecoin-cli stop
+
