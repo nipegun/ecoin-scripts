bb88fa5df1514cc0b200f776ecd786dacd6a8e9d
NiPeGun <nipegun@gmail.com>
2023-05-30 11:13:39 +0200

    Update rvn-gui-iniciar.sh

--- rvn-gui-iniciar.sh
+++ rvn-gui-iniciar.sh
@@ -16,17 +16,15 @@
     vCarpetaHome=${vCarpetaHome%?}
   fi
 
-# Notificar el inicio de ejecución del script
-  echo ""
-  echo "  Iniciando raven-qt..."
-  echo ""
-
 # Parar el daemon
   chmod +x $vCarpetaHome/scripts/c-scripts/rvn-daemon-parar.sh 2> /dev/null
   $vCarpetaHome/scripts/c-scripts/rvn-daemon-parar.sh
   sleep 5
 
 # Iniciar el nodo gráfico
+  echo ""
+  echo "  Iniciando raven-qt..."
+  echo ""
   chmod +x $vCarpetaHome/Cryptos/RVN/bin/raven-cli  2> /dev/null
   chmod +x $vCarpetaHome/Cryptos/RVN/bin/raven-qt   2> /dev/null
   chmod +x $vCarpetaHome/Cryptos/RVN/bin/ravend     2> /dev/null
