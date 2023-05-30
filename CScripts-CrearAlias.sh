a15e3bf3a14f6d68293da7a3ee88331ae09e5e21
NiPeGun <nipegun@gmail.com>
2023-05-30 09:08:47 +0200

    Update CScripts-CrearAlias.sh

--- CScripts-CrearAlias.sh
+++ CScripts-CrearAlias.sh
@@ -5,27 +5,31 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------
-#  Script de NiPeGun para crear los alias de los c-scripts 
-#-----------------------------------------------------------
+# ----------
+# Script de NiPeGun para crear los alias de los c-scripts 
+# ----------
 
-ColorVerde="\033[1;32m"
-FinColor="\033[0m"
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
 
 echo ""
-echo -e "${ColorVerde}Creando alias para los c-scripts...${FinColor}"
+echo -e "${vColorAzulClaro}  Creando alias para los c-scripts...${vFinColor}"
 echo ""
 
-## Ver si la variable de entorno HOME termina con una /
-   CarpetaInst="$HOME"
-   if [[ "$CarpetaInst" == */ ]]; then
-     # Quitarle la /
-     CarpetaInst=${CarpetaInst%?}
-   fi
+# Ver si la variable de entorno HOME termina con una /
+  vCarpetaInst="$HOME"
+  if [[ "$vCarpetaInst" == */ ]]; then
+    # Quitarle la /
+    vCarpetaInst=${vCarpetaInst%?}
+  fi
 
 ln -s $CarpetaInst/scripts/c-scripts/Discos-NVMe-Salud.sh              $CarpetaInst/scripts/c-scripts/Alias/dnvmes
 
 echo ""
-echo -e "${ColorVerde}Alias creados. Deberías poder ejecutar los c-scripts escribiendo el nombre de su alias.${FinColor}"
+echo -e "${vColorVerde}    Alias creados. Deberías poder ejecutar los c-scripts escribiendo el nombre de su alias.${vFinColor}"
 echo ""
 
