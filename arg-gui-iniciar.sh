54161c25417cff7832dd9b427abcf0b256fea541
NiPeGun <nipegun@gmail.com>
2023-09-05 07:34:11 +0200

    Update arg-gui-iniciar.sh

--- arg-gui-iniciar.sh
+++ arg-gui-iniciar.sh
@@ -5,23 +5,28 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------------
-#  Script de NiPeGun para iniciar la cartera gráfica de argentum
-#-----------------------------------------------------------------
+# ----------
+# Script de NiPeGun para iniciar la cartera gráfica de argentum
+# ----------
 
-## Ver si la variable de entorno HOME termina con una /
-   CarpetaHome="$HOME"
-   if [[ "$CarpetaHome" == */ ]]; then
-     # Quitarle la /
-     CarpetaHome=${CarpetaHome%?}
-   fi
+# Ver si la variable de entorno HOME termina con una /
+  CarpetaHome="$HOME"
+  if [[ "$CarpetaHome" == */ ]]; then
+    # Quitarle la /
+    CarpetaHome=${CarpetaHome%?}
+  fi
 
-echo ""
-echo "  Iniciando argentum-qt..."
-echo ""
-chmod +x $CarpetaHome/scripts/c-scripts/arg-daemon-parar.sh 2> /dev/null
-$CarpetaHome/scripts/c-scripts/arg-daemon-parar.sh
-sleep 5
-chmod +x $CarpetaHome/Cryptos/ARG/bin/argentum-qt 2> /dev/null
-$CarpetaHome/Cryptos/ARG/bin/argentum-qt
+# Iniciar el proceso de lanzamiento gráfico del nodo
+  echo ""
+  echo "  Iniciando argentum-qt..."
+  echo ""
+  # Parar primero el daemon
+    chmod +x $CarpetaHome/scripts/c-scripts/arg-daemon-parar.sh 2> /dev/null
+             $CarpetaHome/scripts/c-scripts/arg-daemon-parar.sh
+  # Esperar 5 segundos
+    sleep 5
+  # Asignar permisos de ejecución
+    chmod +x $CarpetaHome/Cryptos/ARG/bin/argentum-qt 2> /dev/null
+  # Lanzar la app gráfica
+    $CarpetaHome/Cryptos/ARG/bin/argentum-qt
 
