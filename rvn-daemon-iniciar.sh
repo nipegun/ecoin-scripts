438dc0794e813128d8ee0086f7e86ad5060a0d45
NiPeGun <nipegun@gmail.com>
2023-09-05 07:05:20 +0200

    Update rvn-daemon-iniciar.sh

--- rvn-daemon-iniciar.sh
+++ rvn-daemon-iniciar.sh
@@ -5,20 +5,21 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------
-#  Script de NiPeGun para iniciar el daemon de raven
-#-----------------------------------------------------
+# ----------
+# Script de NiPeGun para iniciar el daemon de raven
+# ----------
 
-## Ver si la variable de entorno HOME termina con una /
-   CarpetaHome="$HOME"
-   if [[ "$CarpetaHome" == */ ]]; then
-     # Quitarle la /
-     CarpetaHome=${CarpetaHome%?}
-   fi
+# Ver si la variable de entorno HOME termina con una /
+  CarpetaHome="$HOME"
+  if [[ "$CarpetaHome" == */ ]]; then
+    # Quitarle la /
+    CarpetaHome=${CarpetaHome%?}
+  fi
 
-echo ""
-echo "  Iniciando el daemon ravend..."
-echo ""
-chmod +x $CarpetaHome/Cryptos/RVN/bin/ravend 2> /dev/null
-$CarpetaHome/Cryptos/RVN/bin/ravend
+# Iniciar el daemon
+  echo ""
+  echo "  Iniciando el daemon ravend..."
+  echo ""
+  chmod +x $CarpetaHome/Cryptos/RVN/bin/ravend 2> /dev/null
+           $CarpetaHome/Cryptos/RVN/bin/ravend
 
