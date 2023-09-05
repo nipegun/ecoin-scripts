3ffe473802fe9f88c9ed618e8c865d4ba337b839
NiPeGun <nipegun@gmail.com>
2023-09-05 07:03:46 +0200

    Create bch-daemon-parar.sh

---
+++ bch-daemon-parar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para parar el daemon de bitcoin cash
+# ----------
+
+# Ver si la variable de entorno HOME termina con una /
+  CarpetaHome="$HOME"
+  if [[ "$CarpetaHome" == */ ]]; then
+    # Quitarle la /
+    CarpetaHome=${CarpetaHome%?}
+  fi
+
+# Parar el daemon
+  echo ""
+  echo "  Parando el daemon bitcoind..."
+  echo ""
+  chmod +x $CarpetaHome/Cryptos/BCH/bin/bitcoin-cli 2> /dev/null
+           $CarpetaHome/Cryptos/BCH/bin/bitcoin-cli stop
+
