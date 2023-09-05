82c57a523463abf4d9af7961944078cb5dac3b43
NiPeGun <nipegun@gmail.com>
2023-09-05 07:33:49 +0200

    Update dgb-gui-iniciar.sh

--- dgb-gui-iniciar.sh
+++ dgb-gui-iniciar.sh
@@ -5,27 +5,32 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------------
-#  Script de NiPeGun para iniciar la cartera gráfica de digibyte
-#-----------------------------------------------------------------
+# ----------
+# Script de NiPeGun para iniciar la cartera gráfica de digibyte
+# ----------
 
-## Ver si la variable de entorno HOME termina con una /
-   CarpetaHome="$HOME"
-   if [[ "$CarpetaHome" == */ ]]; then
-     # Quitarle la /
-     CarpetaHome=${CarpetaHome%?}
-   fi
+# Ver si la variable de entorno HOME termina con una /
+  CarpetaHome="$HOME"
+  if [[ "$CarpetaHome" == */ ]]; then
+    # Quitarle la /
+    CarpetaHome=${CarpetaHome%?}
+  fi
 
-echo ""
-echo "  Iniciando digibyte-qt..."
-echo ""
-chmod +x $CarpetaHome/scripts/c-scripts/dgb-daemon-parar.sh 2> /dev/null
-$CarpetaHome/scripts/c-scripts/dgb-daemon-parar.sh
-sleep 5
-chmod +x $CarpetaHome/Cryptos/DGB/bin/digibyte-cli  2> /dev/null
-chmod +x $CarpetaHome/Cryptos/DGB/bin/digibyte-qt   2> /dev/null
-chmod +x $CarpetaHome/Cryptos/DGB/bin/digibyte-tx   2> /dev/null
-chmod +x $CarpetaHome/Cryptos/DGB/bin/digibyted     2> /dev/null
-chmod +x $CarpetaHome/Cryptos/DGB/bin/test_digibyte 2> /dev/null
-$CarpetaHome/Cryptos/DGB/bin/digibyte-qt
+# Iniciar el proceso de lanzamiento gráfico del nodo
+  echo ""
+  echo "  Iniciando digibyte-qt..."
+  echo ""
+  # Parar primero el daemon
+    chmod +x $CarpetaHome/scripts/c-scripts/dgb-daemon-parar.sh 2> /dev/null
+             $CarpetaHome/scripts/c-scripts/dgb-daemon-parar.sh
+  # Esperar 5 segundos
+    sleep 5
+  # Asignar permisos de ejecución
+    chmod +x $CarpetaHome/Cryptos/DGB/bin/digibyte-cli  2> /dev/null
+    chmod +x $CarpetaHome/Cryptos/DGB/bin/digibyte-qt   2> /dev/null
+    chmod +x $CarpetaHome/Cryptos/DGB/bin/digibyte-tx   2> /dev/null
+    chmod +x $CarpetaHome/Cryptos/DGB/bin/digibyted     2> /dev/null
+    chmod +x $CarpetaHome/Cryptos/DGB/bin/test_digibyte 2> /dev/null
+  # Lanzar la app gráfica
+    $CarpetaHome/Cryptos/DGB/bin/digibyte-qt
 
