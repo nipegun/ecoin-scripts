19fb5834e0f230d1c473a7926fcb4e613dbf8acd
NiPeGun <nipegun@gmail.com>
2023-09-05 07:32:41 +0200

    Update xmr-gui-iniciar.sh

--- xmr-gui-iniciar.sh
+++ xmr-gui-iniciar.sh
@@ -5,23 +5,28 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#---------------------------------------------------------------
-#  Script de NiPeGun para iniciar la cartera gráfica de monero
-#---------------------------------------------------------------
+# ----------
+# Script de NiPeGun para iniciar la cartera gráfica de monero
+# ----------
 
-## Ver si la variable de entorno HOME termina con una /
-   CarpetaHome="$HOME"
-   if [[ "$CarpetaHome" == */ ]]; then
-     # Quitarle la /
-     CarpetaHome=${CarpetaHome%?}
-   fi
+# Ver si la variable de entorno HOME termina con una /
+  CarpetaHome="$HOME"
+  if [[ "$CarpetaHome" == */ ]]; then
+    # Quitarle la /
+    CarpetaHome=${CarpetaHome%?}
+  fi
 
-echo ""
-echo "  Iniciando monero-gui..."
-echo ""
-chmod +x $CarpetaHome/scripts/c-scripts/xmr-daemon-parar.sh 2> /dev/null
-$CarpetaHome/scripts/c-scripts/xmr-daemon-parar.sh
-sleep 5
-chmod +x $CarpetaHome/Cryptos/XMR/bin/monero-wallet-gui 2> /dev/null
-$CarpetaHome/Cryptos/XMR/bin/monero-wallet-gui %u
+# Iniciar el proceso de lanzamiento gráfico del nodo
+  echo ""
+  echo "  Iniciando monero-gui..."
+  echo ""
+  # Parar primero el demonio
+    chmod +x $CarpetaHome/scripts/c-scripts/xmr-daemon-parar.sh 2> /dev/null
+             $CarpetaHome/scripts/c-scripts/xmr-daemon-parar.sh
+  # Esperar 5 segundos
+    sleep 5
+  # Asignar permisos de ejecución
+    chmod +x $CarpetaHome/Cryptos/XMR/bin/monero-wallet-gui 2> /dev/null
+  # Lanzar la app gráfica
+    $CarpetaHome/Cryptos/XMR/bin/monero-wallet-gui %u
 
