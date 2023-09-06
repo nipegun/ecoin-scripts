ccab426e074283eb6f855bb0b39e11fea9ed86a0
NiPeGun <nipegun@gmail.com>
2023-09-06 19:45:34 +0200

    Update Cryptos-BIP39-CalcularPalabraCheckSum.py

--- Cryptos-BIP39-CalcularPalabraCheckSum.py
+++ Cryptos-BIP39-CalcularPalabraCheckSum.py
@@ -1,10 +1,14 @@
-#!/bin/bash
-
 # Pongo a disposición pública este script bajo el término de "software de dominio público".
 # Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para crear los alias de los c-scripts 
+# Script de NiPeGun para calcular la palabra checksum de una lista de 11 o 23 palabras BIP39
+#
+# El script original es este: https://github.com/91DarioDev/bip39-checksum-words-finder/blob/main/main.py
+#
+# Ejecución remota:
+#  curl -sL x | python3
 # ----------
+
