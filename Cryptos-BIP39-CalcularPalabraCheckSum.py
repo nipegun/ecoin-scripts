7f86d347d252dcb20e0e116369924d562bd96f0f
NiPeGun <nipegun@gmail.com>
2023-09-06 19:51:25 +0200

    Update Cryptos-BIP39-CalcularPalabraCheckSum.py

--- Cryptos-BIP39-CalcularPalabraCheckSum.py
+++ Cryptos-BIP39-CalcularPalabraCheckSum.py
@@ -16,44 +16,48 @@ from hashlib import sha256
 import binascii
 import itertools
 
+
 def get_bip39_words_list():
     lines = None
-    with open('/tmp/BIP39english.txt') as file:
+    # downloaded from https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/english.txt
+    with open('./english.txt') as file:
         lines = [line.rstrip() for line in file]
     assert(len(lines) == 2048)
     return lines
+
 
 def main():
     bip39_words_list = get_bip39_words_list()
 
     while True:
         try:
-            seed_string = input('Inserta las 11 o 23 palabras y presiona Enter:\n').lower()
+            seed_string = input('Insert your first 11 or 23 words and press enter:\n').lower()
         except KeyboardInterrupt:
             return
 
-        # Convertir el string a lista
+        # convert string to list
         seed_list = seed_string.split(' ')
-        # Borrar los espacios en blanco de la lista
+        # remove eventual whitespaces in list
         seed_list = [word for word in seed_list if word]
 
-        # Comprobar que todas las palabras estén en la lista BIP39
+        # check if all words are in bip39 list
         all_words_in_bip39_list = True
         for word in seed_list:
             if word not in bip39_words_list:
                 all_words_in_bip39_list = False
-                print('\nERROR: La palabra {} no está en la lista BIP39\n'.format(word))
-
-        # Evitar continuar si alguna de las palabras están en la lista BIP39
+                print('\nERROR: word {} not in bip39 words list\n'.format(word))
+        
+        # prevent going ahead if not all words in bip39 list
         if not all_words_in_bip39_list:
             continue
 
-        # Comprobar si las palabras insertadas son 11 o 23
+        # check that words inserted must be 11 or 23
         if len(seed_list) != 11 and len(seed_list) != 23:
             print('\nERROR: {} words inserted\n'.format(len(seed_list)))
             continue
 
         break
+
 
     bits_string = ''
     for word in seed_list:
@@ -69,11 +73,11 @@ def main():
     elif len(seed_list) == 23:
         bits_to_add = 3
         chars_for_checksum = 2
-
+    
     combos = itertools.product(['0', '1'], repeat=bits_to_add)
     combos = [ ''.join(list(i)) for i in combos]
     combos = sorted(combos, key=lambda x: int(x, 2))
-
+    
     candidates = '\n\nMISSING BITS - WORD:\n'
     for combo in combos:
         entropy = '{}{}'.format(bits_string, combo)
