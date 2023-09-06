768145e5e175c00b3f8ccc5106add07b4e7bdffe
NiPeGun <nipegun@gmail.com>
2023-09-06 19:56:29 +0200

    Update Cryptos-BIP39-CalcularPalabraCheckSum5.py

--- Cryptos-BIP39-CalcularPalabraCheckSum5.py
+++ Cryptos-BIP39-CalcularPalabraCheckSum5.py
@@ -9,7 +9,7 @@
 # El script original es este: https://github.com/91DarioDev/bip39-checksum-words-finder/blob/main/main.py
 #
 # Ejecución remota:
-#   curl -sL https://raw.githubusercontent.com/nipegun/c-scripts/main/Cryptos-BIP39-CalcularPalabraCheckSum.py | python3
+#   wget https://raw.githubusercontent.com/nipegun/c-scripts/main/Cryptos-BIP39-CalcularPalabraCheckSum.py && python3 Cryptos-BIP39-CalcularPalabraCheckSum.py
 # ----------
 
 from hashlib import sha256
