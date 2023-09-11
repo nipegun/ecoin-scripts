64c9434ae9a16f86be18666aca3f7faa089f0924
NiPeGun <nipegun@gmail.com>
2023-09-11 21:04:46 +0200

    Update xch-Cartera-Notificar-CambioDeSaldo.sh

--- xch-Cartera-Notificar-CambioDeSaldo.sh
+++ xch-Cartera-Notificar-CambioDeSaldo.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-vUsuario="nipegun"
+vUsuarioNoRoot="nipegun"
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
 
 # Definir la ubicación del ejecutable
   echo ""
@@ -12,7 +21,7 @@ vUsuario="nipegun"
     echo "    El ejecutable está en: $vCaminoAlEjecutable"
     echo ""
   else
-    vCaminoAlEjecutable="/home/$vUsuario/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia"
+    vCaminoAlEjecutable="/home/$vUsuarioNoRoot/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia"
     echo ""
     echo "    El ejecutable está en: $vCaminoAlEjecutable"
     echo ""
@@ -21,14 +30,17 @@ vUsuario="nipegun"
   touch /tmp/ChiaSaldo.txt
   chmod 777 /tmp/ChiaSaldo.txt
 # Comprobar cartera
-  su - $vUsuario -c "$vCaminoAlEjecutable wallet show | grep 'Total Balance' | grep -v ending | cut -d':' -f2 | cut -d' ' -f2 > /tmp/ChiaSaldo.txt"
+  su - $vUsuarioNoRoot -c "$vCaminoAlEjecutable wallet show | grep 'Total Balance' | grep -v ending | cut -d':' -f2 | cut -d' ' -f2 > /tmp/ChiaSaldo.txt"
 # Guardar el balance de la cartera en una variable
-  vChiaSaldo=$(cat /tmp/ChiaSaldo.txt)
+  vSaldoCartera=$(cat /tmp/ChiaSaldo.txt)
 # Enviar mensaje si el saldo de la cartera cambia
-  if [[ $vChiaSaldo != "" ]]; then
+  echo ""
+  echo "    El saldo de la cartera es: $vSaldoCartera."
+  echo ""
+  if [[ $vSaldoCartera != "" ]]; then
     #/root/scripts/Telegram-EnviarHTML.sh "<b>Estado de cartera de Chia de <a href='tg://user?id=29421797'>NiPeGun</a>:</b>%0A  Versión de nodo: $ChiaVers %0A  Balance de cartera: $ChiaSaldo %0A  Cantidad de parcelas: $ChiaParc %0A  Espacio de parcelas: $ChiaEspac "
-     /root/scripts/Telegram-EnviarHTML.sh "<b>Balance de la cartera de Chia:</b>%0A  Balance de cartera: $vChiaSaldo %0A "
+     /root/scripts/Telegram-EnviarHTML.sh "<b>Balance de la cartera de Chia:</b>%0A  Balance de cartera: $vSaldoCartera %0A "
      # Actualizar este archvo para adaptar al nuevo saldo
-       sed -i -e 's|$vChiaSaldo != ""|$vChiaSaldo != "'"$vChiaSaldo"'"|g' /root/scripts/xch-Cartera-AvisarDeCambioDeSaldo.sh
+       sed -i -e 's|$vChiaSaldo != ""|$vChiaSaldo != "'"$vSaldoCartera"'"|g' /root/scripts/xch-Cartera-AvisarDeCambioDeSaldo.sh
   fi
 
