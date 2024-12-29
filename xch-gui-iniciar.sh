75b023f81b9278620faf90e94f92ac4258f94ba7
NiPeGun <nipegun@gmail.com>
2024-12-29 19:14:42 +0100

    Update xch-gui-iniciar.sh

--- xch-gui-iniciar.sh
+++ xch-gui-iniciar.sh
@@ -21,8 +21,8 @@
   echo "  Iniciando chia-blockchain..."
   echo ""
   # Parar primero el daemon
-    chmod +x $CarpetaHome/scripts/c-scripts/xch-daemon-parar.sh 2> /dev/null
-             $CarpetaHome/scripts/c-scripts/xch-daemon-parar.sh
+    chmod +x $CarpetaHome/scripts/ecoin-scripts/xch-daemon-parar.sh 2> /dev/null
+             $CarpetaHome/scripts/ecoin-scripts/xch-daemon-parar.sh
   # Esperar 5 segundos
     sleep 5
   # Asignar permisos de ejecución
