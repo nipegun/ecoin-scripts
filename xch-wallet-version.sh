ec7de2bdc1e0d27ba149ac94412b2d200e458a49
NiPeGun <nipegun@gmail.com>
2024-12-29 19:28:05 +0100

    Update xch-wallet-version.sh

--- xch-wallet-version.sh
+++ xch-wallet-version.sh
@@ -9,16 +9,21 @@
 #  Script de NiPeGun para mostrar la version de wallet/nodo de Chia instalado
 # ----------
 
-## Ver si la variable de entorno HOME termina con una /
-   CarpetaHome="$HOME"
-   if [[ "$CarpetaHome" == */ ]]; then
-     # Quitarle la /
-     CarpetaHome=${CarpetaHome%?}
-   fi
+# Ver si la variable de entorno HOME termina con una /
+  CarpetaHome="$HOME"
+  if [[ "$CarpetaHome" == */ ]]; then
+    # Quitarle la /
+    CarpetaHome="${CarpetaHome%?}"
+  fi
 
 echo ""
 echo "  Versión de la cartera de Chia..."
 echo ""
-chmod +x $CarpetaHome/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia
-$CarpetaHome/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia version
-
+# Comprobar en donde está instalado el nodo
+  if [ -f "$CarpetaHome"/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia ]; then
+    chmod +x "$CarpetaHome"/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia
+    "$CarpetaHome"/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia version
+  else
+    chmod +x /opt/chia/resources/app.asar.unpacked/daemon/chia
+    /opt/chia/resources/app.asar.unpacked/daemon/chia version
+  fi
