913c8829faf63e28de6016577235f434a798d52d
NiPeGun <nipegun@gmail.com>
2024-12-29 21:48:50 +0100

    Create xch-daemon-farmer-iniciar.sh

---
+++ xch-daemon-farmer-iniciar.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para iniciar el farmer de chia (Inicia también el harvester, el full_node y la wallet)
+# ----------
+
+# Ver si la variable de entorno HOME termina con una /
+  CarpetaHome="$HOME"
+  if [[ "$CarpetaHome" == */ ]]; then
+    # Quitarle la /
+      CarpetaHome="${CarpetaHome%?}"
+  fi
+
+# Notificar inicio de ejecución del script
+  echo ""
+  echo "  Iniciando el farmer de chia (si es que no está activo)..."
+  echo ""
+
+# Iniciar el nodo en modo daemon
+  # Comprobar en donde está instalado el nodo
+    if [ -f "$CarpetaHome"/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia ]; then
+      chmod +x $CarpetaHome/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia 2> /dev/null
+      $CarpetaHome/Cryptos/XCH/chia-blockchain/resources/app.asar.unpacked/daemon/chia start farmer
+    else
+      chmod +x /opt/chia/resources/app.asar.unpacked/daemon/chia 2> /dev/null
+      /opt/chia/resources/app.asar.unpacked/daemon/chia start farmer
+    fi
+
