6dcd607831b9008139f39ff06c6cb59d7c18b9c7
NiPeGun <nipegun@gmail.com>
2024-12-29 18:20:03 +0100

    Update CScripts-CrearAlias.sh

--- CScripts-CrearAlias.sh
+++ CScripts-CrearAlias.sh
@@ -6,7 +6,7 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para crear los alias de los c-scripts 
+# Script de NiPeGun para crear los alias de los ecoin-scripts 
 # ----------
 
 # Definir variables de color
@@ -17,19 +17,12 @@
   vFinColor='\033[0m'
 
 echo ""
-echo -e "${vColorAzulClaro}  Creando alias para los c-scripts...${vFinColor}"
+echo -e "${vColorAzulClaro}  Creando alias para los ecoin-scripts...${vFinColor}"
 echo ""
 
-# Ver si la variable de entorno HOME termina con una /
-  vCarpetaInst="$HOME"
-  if [[ "$vCarpetaInst" == */ ]]; then
-    # Quitarle la /
-    vCarpetaInst=${vCarpetaInst%?}
-  fi
-
-ln -s $CarpetaInst/scripts/c-scripts/Discos-NVMe-Salud.sh              $CarpetaInst/scripts/c-scripts/Alias/dnvmes
+ln -s ~/scripts/ecoin-scripts/Discos-NVMe-Salud.sh              ~/scripts/ecoin-scripts/Alias/dnvmes
 
 echo ""
-echo -e "${vColorVerde}    Alias creados. Deberías poder ejecutar los c-scripts escribiendo el nombre de su alias.${vFinColor}"
+echo -e "${vColorVerde}    Alias creados. Deberías poder ejecutar los ecoin-scripts escribiendo el nombre de su alias.${vFinColor}"
 echo ""
 
