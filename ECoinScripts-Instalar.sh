919e253957ef87be6282f53bd4acdafab2676630
NiPeGun <nipegun@gmail.com>
2024-12-29 18:46:57 +0100

    Update ECoinScripts-Instalar.sh

--- ECoinScripts-Instalar.sh
+++ ECoinScripts-Instalar.sh
@@ -27,7 +27,18 @@
   echo -e "${vColorAzulClaro}  Iniciando el script de instalación de los ecoin-scripts para el usuario $USER...${vFinColor}"
   echo ""
 
-# Comprobar si hay conexión a Internet antes de sincronizar los c-scripts
+# Comprobar que no exista la carpeta ~/scripts/ecoin-scripts
+  if [ -d ~/scripts/ecoin-scripts ]; then
+    echo ""
+    echo -e "${vColorRojo}    La carpeta ~/scripts/ecoin-scripts/ ya existe. Los ecoin-scripts ya están instalados.${vFinColor}"
+    echo -e "${vColorRojo}    Para sincronizarlo con las últimas versiones, ejecuta:${vFinColor}"
+    echo ""
+    echo -e "${vColorRojo}      ~/scripts/ecoin-scripts/ECoinScripts-Sincronizar.sh${vFinColor}"
+    echo ""
+    exit
+  fi
+
+# Instalar
   # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
     if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
       echo ""
@@ -36,37 +47,32 @@
       sudo apt-get -y update
       sudo apt-get -y install wget
     fi
-  wget -q --tries=10 --timeout=20 --spider https://github.com
-    if [[ $? -eq 0 ]]; then
-      echo ""
-      echo "    Instalando los ecoin-scripts con las últimas versiones y descargando nuevos ecoin-scripts si es que existen..."
-      echo ""
-      rm ~/scripts/ecoin-scripts -R 2> /dev/null
-      mkdir ~/scripts 2> /dev/null
-      cd ~/scripts
-      ## Comprobar si el paquete git está instalado. Si no lo está, instalarlo.
-        if [[ $(dpkg-query -s git 2>/dev/null | grep installed) == "" ]]; then
-          echo ""
-          echo -e "${vColorRojo}      El paquete git no está instalado. Iniciando su instalación...${vFinColor}"
-          echo ""
-          sudo apt-get -y update
-          sudo apt-get -y install git
-          echo ""
-        fi
-      git clone --depth=1 https://github.com/nipegun/ecoin-scripts
-      rm ~/scripts/ecoin-scripts/.git -R 2> /dev/null
-      find ~/scripts/ecoin-scripts/ -type f -iname "*.sh" -exec chmod +x {} \;
-      echo ""
-      echo -e "${vColorVerde}      ecoin-scripts sincronizados correctamente.${vFinColor}"
-      echo ""
-
-      # Crear los alias
-        mkdir -p ~/scripts/ecoin-scripts/Alias/
-        ~/scripts/ecoin-scripts/ECoinScripts-CrearAlias.sh
-        find ~/scripts/ecoin-scripts/Alias -type f -exec chmod +x {} \;
-    else
-      echo ""
-      echo -e "${vColorRojo}    No se pudo iniciar la sincronización de los ecoin-scripts porque no se detectó conexión a Internet.${vFinColor}"
-      echo ""
-    fi
+  # Sincronizar el repositorio
+    echo ""
+    echo "    Sincronizando el repositorio..."
+    echo ""
+    rm ~/scripts/ecoin-scripts -R 2> /dev/null
+    mkdir ~/scripts 2> /dev/null
+    cd ~/scripts
+    # Comprobar si el paquete git está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s git 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo -e "${vColorRojo}      El paquete git no está instalado. Iniciando su instalación...${vFinColor}"
+        echo ""
+        sudo apt-get -y update
+        sudo apt-get -y install git
+        echo ""
+      fi
+    git clone --depth=1 https://github.com/nipegun/ecoin-scripts
+  # Borrar archivos sobrantes
+    rm ~/scripts/ecoin-scripts/.git -R 2> /dev/null
+    find ~/scripts/ecoin-scripts/ -type f -iname "*.sh" -exec chmod +x {} \;
+  # Notificar fin de la instalación
+    echo ""
+    echo -e "${vColorVerde}      ecoin-scripts sincronizados correctamente.${vFinColor}"
+    echo ""
+  # Crear los alias
+    mkdir -p ~/scripts/ecoin-scripts/Alias/
+    ~/scripts/ecoin-scripts/ECoinScripts-CrearAlias.sh
+    find ~/scripts/ecoin-scripts/Alias -type f -exec chmod +x {} \;
 
