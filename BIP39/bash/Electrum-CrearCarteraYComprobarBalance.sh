2ee3dc3ec621ae0d20b3f78c2988133450289662
NiPeGun <nipegun@gmail.com>
2025-09-05 15:55:55 +0200

    Update Electrum-CrearCarteraYComprobarBalance.sh

--- BIP39/bash/Electrum-CrearCarteraYComprobarBalance.sh
+++ BIP39/bash/Electrum-CrearCarteraYComprobarBalance.sh
@@ -20,8 +20,30 @@
 #   curl -sL https://raw.githubusercontent.com/nipegun/ecoin-scripts/refs/heads/main/BIP39/bash/Electrum-CrearCarteraYComprobarBalance.sh | nano -
 # ----------
 
+# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    sudo apt-get -y update
+    sudo apt-get -y install curl
+    echo ""
+  fi
+
+# Comprobar si el paquete jq está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s jq 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete jq no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    sudo apt-get -y update
+    sudo apt-get -y install jq
+    echo ""
+  fi
+
 # Definir variables
   vRutaACarpetaDeCarteras="/mnt/c/Carteras" # Sin / final
+  sudo mkdir -p "$vRutaACarpetaDeCarteras"
+  sudo chmod 777 "$vRutaACarpetaDeCarteras"
 
 # Correr el bucle hasta encontrar balance distinto de cero
   while true; do
